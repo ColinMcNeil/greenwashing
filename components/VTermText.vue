0110e63b36963dd9d3213287ce800f17c3037730
colinmcneil <colinmcneil@gmail.com>
2019-04-29 09:19:10 -0400

    Add infographic

---
+++ components/VTermText.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="term" :class="{hovered}">
+    <div class="text" @mouseover="hovered = true" @mouseleave="hovered = false">
+      <h1 v-show="!hovered">{{before}}</h1>
+      <h1 v-show="hovered">{{after}}</h1>
+      <h4 v-show="hovered">{{subtext}}</h4>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    before: {required: true, type: String},
+    after: {required: true, type: String},
+    subtext: {required: false, type: String, default: ''}
+  },
+  data: ()=>({hovered:false})
+}
+</script>
+<style lang="scss" scoped>
+@import '~/assets/sass/_variables.scss';
+.term{
+  padding: 1em;
+  box-sizing: border-box;
+}
+.text {
+  
+  &:hover{
+   h1, h4{
+     color: yellow;
+   }
+ } 
+}
+.term.hovered {
+  background-color: $dark-brown;
+}
+
+</style>
---
+++ pages/info.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <div class="terms">
+      <h1>Terms</h1>
+      <v-term-text 
+      before="Greenwashing" 
+      after="Is the way companies market eco-friendly 
+      products that in practice affect the environment the same or worse."
+      subtext="30% of our respondents believed it meant washing in an eco-friendly way"/>
+      <v-term-text before="All Natural" 
+      after="Is not regulated by the FDA. Anything can claim to be 
+      'All Natural'"
+      subtext="Only 20% of respondents knew there are no enforced definitions."/>
+      <v-term-text before="Natural Products" 
+      after="Are not necessarily better for the environment. 
+      Check the labels before buying."
+      subtext="60% of respondents believed all-natural products were better for the environment"/>
+    </div>
+    <div class="products">
+      <h1>Examples of greenwashed products</h1>
+    </div>
+  </div>
+</template>
+<script>
+import VTermText from '~/components/VTermText'
+export default {
+  components: {VTermText},
+  data: ()=>({hovered:''}),
+}
+</script>
+<style lang="scss" scoped>
+@import '~/assets/sass/_variables.scss';
+
+.container{
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  font-size: 1.5em;
+  * {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: $light-green;
+  }
+  .products {
+    top: 100vh;
+    justify-content: flex-start;
+  } 
+}
+</style>
